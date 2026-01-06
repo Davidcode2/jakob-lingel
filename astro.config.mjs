7a9f47dec5866327040fe236d9ae46332d045f98
Jakob Lingel <jlingel68@gmail.com>
2026-01-06 21:14:38 +0100

    Iconify, Description block

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon(
+    { 
+      include: 
+      {
+        lucide: ["*"],
+        "simple-icons": ["*"],
+      },
+    }
+  )]
 });
